76df7ac99552c543d0258415e34dcfbfbdcafc8d
Sunil-MI <sunillulla546@yahoo.com>
2017-07-07 16:28:27 +0530

    changes in layout for ratings

--- controllers/shoeMartCtrl.js
+++ controllers/shoeMartCtrl.js
@@ -1,7 +1,10 @@
 shoeapp.controller('shoeMartCtrl', function($scope, ShoeData) {
     // $scope.firstName = "John";
-    console.log(ShoeData.get_shoe_all_data());
+    // console.log(ShoeData.get_shoe_all_data());
     ShoeData.get_shoe_all_data().then(function(resp){
     	$scope.shoes_data = resp;
     });
+    $scope.range = function(n) {
+        return new Array(n);
+    };
 });
